273648b4e9f661e76955fe9a4bb8d91d94060107
Ivan Uresti <ivan.uresti@gmail.com>
2022-04-07 08:01:47 -0500

    prepare exercise 2022A

--- src/test/java/com/nearsoft/training/library/service/impl/LoanServiceImplTest.java
+++ src/test/java/com/nearsoft/training/library/service/impl/LoanServiceImplTest.java
@@ -1,5 +1,16 @@
 package com.nearsoft.training.library.service.impl;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import com.nearsoft.training.library.config.LoanConfigurationProperties;
 import com.nearsoft.training.library.exception.LoanNotAllowedException;
 import com.nearsoft.training.library.model.Book;
@@ -8,16 +19,6 @@ import com.nearsoft.training.library.model.User;
 import com.nearsoft.training.library.repository.BookRepository;
 import com.nearsoft.training.library.service.CardReaderService;
 import com.nearsoft.training.library.service.UserService;
-import org.junit.Test;
-import org.mockito.Mockito;
-
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LoanServiceImplTest {
 
@@ -82,48 +83,8 @@ public class LoanServiceImplTest {
         loanConfigurationProperties.setMaxBooksPerUser(2);
 
         // When:
-        assertThatThrownBy(() -> loanService.lendBooks(isbnList))
-                .isInstanceOf(LoanNotAllowedException.class)
-                .hasMessage("Attempt to borrow more than once a book");
+        assertThatThrownBy(() -> loanService.lendBooks(isbnList)).isInstanceOf(LoanNotAllowedException.class).hasMessage("Attempt to borrow more than once a book");
 
     }
 
-
-    @Test
-    public void givenAUserWithExpiredLoans_whenLendBooksForAnExistentBook_thenThrowException() {
-        // Given:
-        CardReaderService cardReaderService = Mockito.mock(CardReaderService.class);
-        UserService userService = Mockito.mock(UserService.class);
-        LoanConfigurationProperties loanConfigurationProperties = new LoanConfigurationProperties();
-        BookRepository bookRepository = Mockito.mock(BookRepository.class);
-        LoanServiceImpl loanService = new LoanServiceImpl(cardReaderService, userService, loanConfigurationProperties, bookRepository);
-        String[] isbnList = {"ABC", "DEF"};
-        User user = new User();
-        String curp = UUID.randomUUID().toString();
-        Set<BooksByUser> borrowedBooks = new HashSet<>();
-        Book abcBook = new Book();
-        BooksByUser booksByUserForABC = new BooksByUser();
-        LocalDate now = LocalDate.now();
-
-        user.setCurp(curp);
-
-        booksByUserForABC.setCurp(curp);
-        booksByUserForABC.setIsbn("OBC");
-        booksByUserForABC.setBorrowDate(now.minusDays(45));
-
-        borrowedBooks.add(booksByUserForABC);
-
-        Mockito.when(cardReaderService.readUser()).thenReturn(user);
-        Mockito.when(userService.getBorrowedBooks(curp)).thenReturn(borrowedBooks);
-        Mockito.when(bookRepository.findById("ABC")).thenReturn(Optional.of(abcBook));
-        Mockito.when(bookRepository.findById("DEF")).thenReturn(Optional.empty());
-
-        loanConfigurationProperties.setMaxBooksPerUser(2);
-
-        // When:
-        assertThatThrownBy(() -> loanService.lendBooks(isbnList))
-                .isInstanceOf(LoanNotAllowedException.class)
-                .hasMessage("User must return old loans");
-
-    }
 }
